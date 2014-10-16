23430dd88b704babfae73f98821a6ecd873a5c65
Richard Siwady <rsiwady29@gmail.com>
2014-10-15 19:08:30 -0600

    adding yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name eventosEnHondurasApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the eventosEnHondurasApp
+ */
+angular.module('eventosEnHondurasApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
